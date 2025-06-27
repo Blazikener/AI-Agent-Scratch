de49e09250aa7c548bdb781b1f673ed898ffe250
Yasir <147266162+Blazikener@users.noreply.github.com>
2025-06-27 14:53:13 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+#from tools import search_tool, wiki_tool, save_tool
+import os
+
+load_dotenv()
+
+class research_query(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile", 
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+print("NO Errors so far")prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+
