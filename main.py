d335d715df9a225525938855031294d583b23a85
Yasir <147266162+Blazikener@users.noreply.github.com>
2025-06-28 13:29:23 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-#from tools import search_tool, wiki_tool, save_tool
+from tools import search_tool, wiki_tool, save_tool
 import os
 
 load_dotenv()
@@ -20,7 +20,9 @@ llm = ChatGroq(
     api_key=os.getenv("GROQ_API_KEY")
 )
 
-print("NO Errors so far")prompt = ChatPromptTemplate.from_messages(
+parser = PydanticOutputParser(pydantic_object=research_query)
+
+prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -37,3 +39,19 @@ print("NO Errors so far")prompt = ChatPromptTemplate.from_messages(
 ).partial(format_instructions=parser.get_format_instructions())
 
 
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can i help you research? ")
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
